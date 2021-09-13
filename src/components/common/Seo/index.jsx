a04618f79fa827211ac8e30fb301a8ec2dd43276
Michal Bodzianowski <michaljbodzianowski@gmail.com>
2021-09-13 11:52:50 -0600

    move gtag script

--- src/components/common/Seo/index.jsx
+++ src/components/common/Seo/index.jsx
@@ -11,8 +11,7 @@ import {
 	contact,
 	legalName,
 	foundingDate,
-	logo,
-	googleAnalyticsID
+	logo
 } from "data/config";
 
 export const Seo = ({
@@ -78,14 +77,6 @@ export const Seo = ({
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 			<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
 			<title>{title}</title>
-			<script>{`
-				window.gtag('event', 'page_view', {
-					'page_title': 'Home Page',
-					page_location: 'Default',
-					page_path: '/',
-					send_to:'`+{googleAnalyticsID}+`'
-				});
-			`}</script>
 			<html lang="en" dir="ltr" />
 		</Helmet>
 	);
--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -5,12 +5,24 @@ import { Header } from 'components/theme';
 import { Container, Button } from 'components/common';
 import dev from 'assets/illustrations/banner.png';
 import { Wrapper, IntroWrapper, Details, Thumbnail } from './styles';
+import { Helmet } from 'react-helmet';
+import { googleAnalyticsID } from 'data/config';
 
 export const Intro = () => {
   const { theme } = useContext(ThemeContext);
 
   return (
     <Wrapper>
+      <Helmet>
+        <script>{`
+				gtag('event', 'page_view', {
+					'page_title': 'Home Page',
+					page_location: 'Default',
+					page_path: '/',
+					send_to:'`+ { googleAnalyticsID } + `'
+				});
+			`}</script>
+      </Helmet>
       <Header />
       <IntroWrapper as={Container}>
         <Details theme={theme}>
